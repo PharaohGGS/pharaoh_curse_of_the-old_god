c2f9501d5b857326affdf484682fafe0374d78fb
kryffin <nicolaskleinhentz@gmail.com>
2022-02-03 11:35:27 +0100

    added dash using a coroutine, limited to one use in-air

--- pharaoh/Assets/Game/Scripts/PlayerMovement.cs
+++ pharaoh/Assets/Game/Scripts/PlayerMovement.cs
@@ -11,11 +11,14 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _smoothMovement;
     private bool _isRunning = false;
     private bool _isFacingRight = true;
+    private bool _isDashing = false;
+    private bool _hasDashedInAir = false;
 
     [Header("Key Bindings")]
 
     public InputAction movementInput;
     public InputAction jumpInput;
+    public InputAction dashInput;
 
     [Header("Movement metrics")]
 
@@ -26,6 +29,12 @@ public class PlayerMovement : MonoBehaviour
 
     [Tooltip("15.1 to get a 3m70 jump (feet position)")]
     public float jumpForce = 15.1f;
+
+    [Tooltip("Dashing force, 50 works well")]
+    public float dashForce = 5f;
+
+    [Tooltip("Dashing time, 0.1 works well")]
+    public float dashTime = 0.1f;
 
     [Tooltip("Smooths the player movement, 0.03 works well")]
     public float smoothInput = 0.03f;
@@ -45,7 +54,6 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        jumpInput.performed += Jump;
     }
 
     private void Update()
@@ -53,6 +61,23 @@ public class PlayerMovement : MonoBehaviour
         _movement = movementInput.ReadValue<Vector2>();
         var vel = Vector2.zero; //unused but necessary
         _smoothMovement = Vector2.SmoothDamp(_smoothMovement, _movement, ref vel, smoothInput); //smooths the input to get a movement acceleration
+
+        if (jumpInput.triggered && IsGrounded() && !_isDashing)
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+
+        if (dashInput.triggered && !_isDashing && !_hasDashedInAir)
+        {
+            _rigidbody.velocity = new Vector2(dashForce * Mathf.Sign(_smoothMovement.x), _rigidbody.velocity.y);
+            _isDashing = true;
+
+            if (!IsGrounded())
+                _hasDashedInAir = true;
+
+            StartCoroutine(Dashing());
+        }
+
+        if (_hasDashedInAir && IsGrounded())
+            _hasDashedInAir = false;
 
         // Cuts off the smoothdamp movement when decelerating
         if (_movement.x == 0f && _smoothMovement.x > -movementDeadRange && _smoothMovement.x < movementDeadRange)
@@ -73,18 +98,12 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // Moves the player horizontally with different speeds whil in-air
-        if (IsGrounded())
-            _rigidbody.velocity = new Vector2(_smoothMovement.x * horizontalSpeed, _rigidbody.velocity.y);
-        else
-            _rigidbody.velocity = new Vector2(_smoothMovement.x * inAirHorizontalSpeed, _rigidbody.velocity.y);
-    }
-
-    // Makes the player jump if he's grounded
-    private void Jump(InputAction.CallbackContext ctx)
-    {
-        if (IsGrounded())
+        if (!_isDashing)
         {
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+            if (IsGrounded())
+                _rigidbody.velocity = new Vector2(_smoothMovement.x * horizontalSpeed, _rigidbody.velocity.y);
+            else
+                _rigidbody.velocity = new Vector2(_smoothMovement.x * inAirHorizontalSpeed, _rigidbody.velocity.y);
         }
     }
 
@@ -92,6 +111,27 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    System.Collections.IEnumerator Dashing()
+    {
+        yield return new WaitForSeconds(dashTime);
+
+        _isDashing = false;
+    }
+
+    private void OnEnable()
+    {
+        movementInput.Enable();
+        jumpInput.Enable();
+        dashInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movementInput.Disable();
+        jumpInput.Disable();
+        dashInput.Disable();
     }
 
     private void OnDrawGizmosSelected()
@@ -113,22 +153,12 @@ public class PlayerMovement : MonoBehaviour
         Gizmos.DrawLine(_rigidbody.position, _rigidbody.position + _rigidbody.velocity);
 
         //Stats
+        Handles.Label(_rigidbody.position + Vector2.up * 3f, "IsDashing : " + _isDashing, _isDashing ? greenStyle : redStyle);
+        Handles.Label(_rigidbody.position + Vector2.up * 2.8f, "HasDashedInAir : " + _hasDashedInAir, _hasDashedInAir ? greenStyle : redStyle);
         Handles.Label(_rigidbody.position + Vector2.up * 2.6f, "IsRunning : " + _isRunning, _isRunning ? greenStyle : redStyle);
         Handles.Label(_rigidbody.position + Vector2.up * 2.4f, "IsFacingRight : " + _isFacingRight, _isFacingRight ? greenStyle : redStyle);
         Handles.Label(_rigidbody.position + Vector2.up * 2.2f, "IsGrounded : " + IsGrounded(), IsGrounded() ? greenStyle : redStyle);
         Handles.Label(_rigidbody.position + Vector2.up * 2f, "Speed : " + _rigidbody?.velocity.magnitude + " m/s", _rigidbody.velocity.magnitude != 0f ? greenStyle : redStyle);
-    }
-
-    private void OnEnable()
-    {
-        movementInput.Enable();
-        jumpInput.Enable();
-    }
-
-    private void OnDisable()
-    {
-        movementInput.Disable();
-        jumpInput.Disable();
     }
 
 }
