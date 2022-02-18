fa470a03175ddd967efef9838e15279d10f70c09
NathanRaki <nathan.raklure@gmail.com>
2022-02-18 01:53:53 +0100

    Added ugly blur to adjacent rooms.

--- pharaoh/Assets/Game/Scripts/Room.cs
+++ pharaoh/Assets/Game/Scripts/Room.cs
@@ -7,15 +7,42 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public GameObject virtualCamera;
+    public SpriteRenderer hider;
+
+    private float _timer;
+    private Coroutine _fading;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         virtualCamera.SetActive(true);
+        if (_fading != null) StopCoroutine(_fading);
+        _fading = StartCoroutine(Fade(0f, .3f));
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         virtualCamera.SetActive(false);
+        if (_fading != null) StopCoroutine(_fading);
+        _fading = StartCoroutine(Fade(1f, .3f));
+    }
+
+    private IEnumerator Fade(float alpha, float time)
+    {
+        Color hiderColor = hider.color;
+        float startAlpha = hiderColor.a;
+        float progress = startAlpha < alpha ? (startAlpha / alpha) : (alpha / startAlpha);
+        float timeToFade = time - time * progress;
+        float elapsedTime = 0f;
+        while (elapsedTime < timeToFade)
+        {
+            hiderColor.a = Mathf.Lerp(startAlpha, alpha, elapsedTime / timeToFade);
+            hider.color = hiderColor;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        hiderColor.a = alpha;
+        hider.color = hiderColor;
+        yield return null;
     }
     
     // private void OnTriggerEnter2D(Collider2D col)
