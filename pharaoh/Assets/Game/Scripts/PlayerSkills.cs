42fef2cff46030cb43434c744c0491b9e6059e09
NathanRaki <nathan.raklure@gmail.com>
2022-02-18 22:33:03 +0100

    Finished vfx dash animation.

---
+++ pharaoh/Assets/Game/Scripts/PlayerSkills.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.VFX;
+
+public class PlayerSkills : MonoBehaviour
+{
+    
+    public InputAction action;
+    public LayerMask layers;
+    public float teleportMinusDelay = 0f;
+    
+    private VisualEffect _teleportVFX;
+    private PlayerMovement _playerMovement;
+
+
+    private void OnEnable()
+    {
+        action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
+    }
+
+    void Start()
+    {
+        if (transform.TryGetComponent(out VisualEffect vfx))
+            _teleportVFX = vfx;
+        if (transform.TryGetComponent(out PlayerMovement playerMovement))
+            _playerMovement = playerMovement;
+        action.performed += _ => Teleport();
+    }
+
+    private void Teleport()
+    {
+        StartCoroutine(TeleportCoroutine());
+    }
+
+    private IEnumerator TeleportCoroutine()
+    {
+        action.Disable();
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        RigidbodyConstraints2D initialConstraints = rb.constraints;
+        rb.constraints = initialConstraints | RigidbodyConstraints2D.FreezePosition;
+        
+        _playerMovement.TogglePlayerInput(false);
+        transform.Find("Model").gameObject.SetActive(false);
+        
+        Vector3 target = _teleportVFX.GetVector3("TargetPosition");
+        float z = transform.Find("Model").localScale.z;
+        if (Math.Abs(Mathf.Sign(target.z) - Mathf.Sign(z)) > 0.1f)
+            target *= -1;
+        _teleportVFX.SetVector3("TargetPosition", target);
+        
+        _teleportVFX.Play();
+
+        float[] delays =
+        {
+            _teleportVFX.GetFloat("DissolveLifetime"),
+            _teleportVFX.GetFloat("DashLifetime"),
+            _teleportVFX.GetFloat("RespawnLifetime")
+        };
+        yield return new WaitForSeconds(delays.Sum() - teleportMinusDelay);
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            new Vector2(target.z, target.y),
+            target.magnitude,
+            layers);
+
+        if (hit)
+            transform.position = hit.point;
+        else
+            transform.position += new Vector3(target.z, target.y, target.x);
+        
+        transform.Find("Model").gameObject.SetActive(true);
+        _playerMovement.TogglePlayerInput(true);
+        rb.constraints = initialConstraints;
+        action.Enable();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, new Vector3(3 * transform.Find("Model").localScale.z, 0, 0));
+    }
+}
