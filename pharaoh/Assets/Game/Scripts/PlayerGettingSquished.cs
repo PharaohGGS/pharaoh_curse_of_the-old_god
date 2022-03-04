5e9541b8f8f4153a9f6394a54ee8bcb02205558a
kryffin <nicolaskleinhentz@gmail.com>
2022-03-04 15:29:05 +0100

    moved the fade code to its unique script on the RawImage object

--- pharaoh/Assets/Game/Scripts/PlayerGettingSquished.cs
+++ pharaoh/Assets/Game/Scripts/PlayerGettingSquished.cs
@@ -1,26 +1,21 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerGettingSquished : MonoBehaviour
 {
 
     private Rigidbody2D _rigidBody;
+    private FadeToBlack _fade;
 
     [Tooltip("What layer is the Moving Block ?")]
     public LayerMask whatIsMovingBlock;
     [Tooltip("Transform at which the player will respawn")]
     public Transform DEBUGRespawnPoint;
-    [Tooltip("Raw Image used to fade to black when respawning")]
-    public RawImage blackScreen;
-    [Tooltip("Duration of the fade animation")]
-    public float fadeDuration;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
-        blackScreen.color = Color.black;
-        blackScreen.canvasRenderer.SetAlpha(0f);
+        _fade = FindObjectOfType<FadeToBlack>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,8 +23,7 @@ public class PlayerGettingSquished : MonoBehaviour
         if (whatIsMovingBlock == (whatIsMovingBlock | (1 << collision.gameObject.layer)) && collision.gameObject.GetComponent<Rigidbody2D>().velocity.y < -0.1f)
         {
             // Damage player and spawn him back to spawnpoint
-            blackScreen.canvasRenderer.SetAlpha(1f);
-            blackScreen.CrossFadeAlpha(0f, fadeDuration, false);
+            _fade.Fade();
 
             //damage player
             Debug.LogWarning("PlayerGettingSquished : need to implement the health component to damage (1) the player");
