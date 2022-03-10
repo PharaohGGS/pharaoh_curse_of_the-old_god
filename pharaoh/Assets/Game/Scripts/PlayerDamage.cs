ce6a08af07db9a919a7117da5997a717ddfa5d4f
Donnovan Feuillastre <donnovan42320@gmail.com>
2022-03-10 14:07:13 +0100

    - FadeToBlack.prefab - AbstractGameEvent for void - FadeToBlack listen to OnRespawnGameEvent for fading

--- pharaoh/Assets/Game/Scripts/PlayerDamage.cs
+++ pharaoh/Assets/Game/Scripts/PlayerDamage.cs
@@ -1,3 +1,4 @@
+using Pharaoh.GameEvents;
 using Pharaoh.Tools;
 using UnityEngine;
 using UnityEngine.Events;
@@ -5,10 +6,7 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerDamage : MonoBehaviour
 {
-    private PlayerMovement _playerMovement;
-    private Pharaoh.Gameplay.HookTargeting _hookTargeting;
     private Rigidbody2D _rigidBody;
-    private FadeToBlack _fade;
     private bool _isRespawning = false;
 
     [Tooltip("What layer is the Moving Block ?")]
@@ -27,10 +25,7 @@ public class PlayerDamage : MonoBehaviour
 
     private void Awake()
     {
-        _playerMovement = GetComponent<PlayerMovement>();
-        _hookTargeting = GetComponent<Pharaoh.Gameplay.HookTargeting>();
         _rigidBody = GetComponent<Rigidbody2D>();
-        _fade = FindObjectOfType<FadeToBlack>();
 
         if (DEBUGRespawnPoint == null)
         {
@@ -41,19 +36,6 @@ public class PlayerDamage : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Block falling on the player
-        //if (whatIsMovingBlock.HasLayer(collision.gameObject.layer) && collision.gameObject.GetComponent<Rigidbody2D>().velocity.y < -0.1f)
-        //    Respawn();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //if (whatIsSpike.HasLayer(collision.gameObject.layer))
-        //    RespawnCoroutine();
-    }
-
     public void MoveToSpawnPoint()
     {
         _rigidBody.position = DEBUGRespawnPoint.position;
@@ -62,12 +44,6 @@ public class PlayerDamage : MonoBehaviour
     public void Respawn()
     {
         if (_isRespawning) return; //the player is already respawning due to damage
-
-        // Damage player
-        Debug.Log("PlayerDamage : need to implement the health component to damage (1) the player");
-
-        //_playerMovement.enabled = false;
-
         StartCoroutine(RespawnCoroutine());
     }
 
@@ -78,14 +54,7 @@ public class PlayerDamage : MonoBehaviour
         yield return new WaitForSeconds(delayBeforeRespawn);
         
         // Fade the screen and respawn the player
-        _fade.Fade();
-        //_rigidBody.position = DEBUGRespawnPoint.position;
         onRespawn?.Invoke();
-
-        //_playerMovement.enabled = true;
-        //_playerMovement.RespawnCoroutine();
-        //_hookTargeting.RespawnCoroutine();
-
         _isRespawning = false;
     }
 
