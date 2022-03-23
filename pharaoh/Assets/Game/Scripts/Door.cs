43e8f5fe354353570d9a00b2aeb29ba551700de8
kryffin <nicolaskleinhentz@gmail.com>
2022-03-23 16:57:12 +0100

    added animations with clipping door

--- pharaoh/Assets/Game/Scripts/Door.cs
+++ pharaoh/Assets/Game/Scripts/Door.cs
@@ -7,14 +7,28 @@ public class Door : MonoBehaviour
     private BoxCollider2D _boxCollider;
     private MeshRenderer _meshRenderer;
     private int _activePlates = 0;
+    private Animator _animator;
 
     [Tooltip("An inverted door means an active pressure plate will close it.")]
     public bool inverted = false;
+
+    [Tooltip("Clipping Material")]
+    public Material _material;
+    public GameObject door;
+    public GameObject firstScarab;
+    public GameObject secondScarab;
 
     private void Awake()
     {
         _boxCollider = GetComponent<BoxCollider2D>();
         _meshRenderer = transform.Find("Skin").GetComponent<MeshRenderer>();
+        _animator = GetComponent<Animator>();
+
+        _material.SetFloat("_Clip", transform.position.y - 1.5f);
+
+        door.GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(_material);
+        firstScarab.GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(_material);
+        secondScarab.GetComponent<MeshRenderer>().material.CopyPropertiesFromMaterial(_material);
 
         RefreshState();
     }
@@ -38,8 +52,12 @@ public class Door : MonoBehaviour
     // Refreshes the state of the door, whether it is open or not
     private void RefreshState()
     {
-        _boxCollider.enabled = !IsOpen();
-        _meshRenderer.enabled = !IsOpen();
+        bool isOpen = IsOpen();
+
+        _boxCollider.enabled = !isOpen;
+        _meshRenderer.enabled = !isOpen;
+
+        _animator.SetBool("IsOpen", isOpen);
     }
 
     // Returns whether the door is open or not
