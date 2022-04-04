d35bdddc86d9e3ddf7a34abdfc3e8a67ac65b255
Donnovan Feuillastre <donnovan42320@gmail.com>
2022-04-04 15:58:40 +0200

    - stun when dashing, hitting an EnemyAgent

---
+++ pharaoh/Assets/Game/Scripts/Runtime/AI/Conditions/Attacks/CheckStun.cs
@@ -0,0 +1,26 @@
+﻿using BehaviourTree.Tools;
+using Pharaoh.Gameplay.Components;
+using Pharaoh.Tools.Debug;
+
+namespace Pharaoh.AI.Actions
+{
+    public class CheckStun : ActionNode
+    {
+        private AiMovement _aiMovement; 
+        
+        protected override void OnStart()
+        {
+            if (_aiMovement) return;
+
+            if (agent.TryGetComponent(out _aiMovement)) return;
+
+            LogHandler.SendMessage($"[{agent.name}] Can't be stunned", MessageType.Warning);
+        }
+
+        protected override NodeState OnUpdate()
+        {
+            if (!_aiMovement) return NodeState.Failure;
+            return _aiMovement.isStunned ? NodeState.Success : NodeState.Failure;
+        }
+    }
+}
---
+++ pharaoh/Assets/Game/Scripts/Runtime/Components/Movement/Data/StunData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Pharaoh.Gameplay.Components
+{
+    [CreateAssetMenu(fileName = "New Stun Data", menuName = "Data/Stun")]
+    public class StunData : ScriptableObject
+    {
+        public string type;
+        public float time;
+    }
+}
