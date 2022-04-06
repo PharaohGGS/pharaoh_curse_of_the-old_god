a9f2bbd66d2c290290c5ccc95fa400a21bbab93a
kryffin <nicolaskleinhentz@gmail.com>
2022-04-06 12:21:26 +0200

    reworked the levelManager a bit, working with all the game scenes

--- pharaoh/Assets/Game/Scripts/Managers/LevelManager.cs
+++ pharaoh/Assets/Game/Scripts/Managers/LevelManager.cs
@@ -1,6 +1,22 @@
-using DesignPatterns;
+using UnityEngine;
 
-public class LevelManager : MonoSingleton<LevelManager>
+public class LevelManager : MonoBehaviour
 {
+
+    public static LevelManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public string currentRoom; // Stores the current room scene name
 }
