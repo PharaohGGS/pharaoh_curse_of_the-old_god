2ead190673c7779a4bdc3244d70b2a3e0c6a5724
Donnovan Feuillastre <donnovan42320@gmail.com>
2022-04-08 14:02:54 +0200

    - fix trap synchro & delay

--- pharaoh/Assets/Game/Scripts/Runtime/Components/Trap/TrapCapacity.cs
+++ pharaoh/Assets/Game/Scripts/Runtime/Components/Trap/TrapCapacity.cs
@@ -21,7 +21,7 @@ namespace Pharaoh.Gameplay
         {
             // check if the current target is different (I mean null here)
             // don't start trap when there isn't any target or already processing
-            if (_behaviour.isStarted || !_detection.TryGetByIndex(0, out GameObject target)) return;
+            if (_behaviour.isStarted || !_detection.TryGetByIndex(0, out GameObject target) || !target) return;
             _behaviour.Activate(target);
         }
     }
