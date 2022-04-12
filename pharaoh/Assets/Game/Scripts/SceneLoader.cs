06e08ca23a535fc2720245bbda1ccd58701166e9
kryffin <nicolaskleinhentz@gmail.com>
2022-04-12 15:54:49 +0200

    added Mylog script to debug build version

--- pharaoh/Assets/Game/Scripts/SceneLoader.cs
+++ pharaoh/Assets/Game/Scripts/SceneLoader.cs
@@ -36,32 +36,41 @@ public class SceneLoader : MonoBehaviour
 
     private void OnRoomChanged()
     {
+        Debug.Log("1");
         if (LevelManager.Instance == null)
             return;
-
+        Debug.Log("2");
         // Get the GameObject which represents the current room
         GameObject currentRoom = GameObject.Find(LevelManager.Instance.currentRoom);
-
+        Debug.Log("3");
         if (!currentRoom.TryGetComponent(out SceneLoader sceneLoader)) return; // If it has no SceneLoader, return
-
+        Debug.Log(scene.name);
+        Debug.Log("4");
+        Debug.Log(currentRoom.name);
         // If this room is in CurrentRoom's neighbours or is the CurrentRoom, load it
         if (scene.name == currentRoom.name)
         {
             if (_isLoaded) return;
             _isLoaded = true;
+            Debug.Log("5");
             SceneManager.LoadScene(scene.name, LoadSceneMode.Additive);
+            Debug.Log("6");
         }
         else if (sceneLoader.neighbours.Contains(scene))
         {
             if (_isLoaded) return;
             _isLoaded = true;
+            Debug.Log("7");
             StartCoroutine(LoadScene());
+            Debug.Log("8");
         }
         else
         {
             if (!_isLoaded) return;
             _isLoaded = false;
+            Debug.Log("9");
             StartCoroutine(UnloadScene());
+            Debug.Log("10");
         }
     }
 
