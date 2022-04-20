9fd702a1b21218d726250f6c1131eedc86d2f04c
Donnovan Feuillastre <donnovan42320@gmail.com>
2022-04-20 10:55:15 +0200

    - add knockback when being hit

--- pharaoh/Assets/Game/Scripts/Runtime/Components/Attack/Damager.cs
+++ pharaoh/Assets/Game/Scripts/Runtime/Components/Attack/Damager.cs
@@ -15,16 +15,27 @@ namespace Pharaoh.Gameplay.Components
         public DamagerData damagerData;
         public StunData stunData;
 
+        public Vector2 enterOffsetPosition { get; private set; }
+
         [SerializeField] private LayerMask damagingLayers;
         [HideInInspector] public UnityEvent<Damager, Collider2D> onTriggerHit;
 
         [SerializeField] private LayerMask collidingLayers;
         [HideInInspector] public UnityEvent<Damager, Collider2D> onCollisionHit;
-        
+
+        private Collider2D _collider;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (gameObject.IsCollidingHimself(other, true)) return;
-            
+
+            enterOffsetPosition = transform.TransformPoint(_collider.offset);
+
             if (other.gameObject.HasLayer(damagingLayers))
             {
                 onTriggerHit?.Invoke(this, other);
