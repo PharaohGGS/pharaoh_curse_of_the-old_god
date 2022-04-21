05edffd8bbd04f86b036e4ce55b56a66b68af390
Donnovan Feuillastre <donnovan42320@gmail.com>
2022-04-21 12:06:18 +0200

    - dash through enemy layer (Player - Dash) - damage deal on enemy when dashing with drawing swords - combat phase with modulo, you can add other attacks (name = "Attack{combatPhase + 1}")

--- pharaoh/Assets/Game/Scripts/Runtime/Components/Movement/PlayerMovement.cs
+++ pharaoh/Assets/Game/Scripts/Runtime/Components/Movement/PlayerMovement.cs
@@ -31,6 +31,7 @@ namespace Pharaoh.Gameplay.Components.Movement
         private float _smoothInput = 0.03f;
         private float _turnSpeed = 7f; //value defined with Cl?mence
         private int _defaultLayer;
+        private int _dashLayer;
         private int _swarmDashLayer;
         private bool _isRunning = false;
         private bool _isDashing = false;
@@ -88,12 +89,14 @@ namespace Pharaoh.Gameplay.Components.Movement
         [SerializeField] private LayerMask dashStunLayer;
         [SerializeField] private StunData dashStunData;
         [SerializeField] private UnityEvent<GameObject, StunData> onDashStun;
+        [SerializeField] private UnityEvent onDashEnd;
 
         private bool _canJumpHook;
 
         private void Awake()
         {
             _defaultLayer = LayerMask.NameToLayer("Player");
+            _dashLayer = LayerMask.NameToLayer("Player - Dash");
             _swarmDashLayer = LayerMask.NameToLayer("Player - Swarm");
             _rigidbody = GetComponent<Rigidbody2D>();
             inputReader.Initialize(); //need to manually initialize
@@ -188,6 +191,8 @@ namespace Pharaoh.Gameplay.Components.Movement
                 _isDashing = true;
                 inputReader.DisableDash();
                 _isHooked = false;
+
+                gameObject.layer = _dashLayer;
 
                 if (skills.hasSwarmDash)
                 {
